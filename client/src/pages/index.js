ccddbe1f2596ed8edf28d88973d1a0c9c1bef797
Carl Aiau <carl@ortus.co.nz>
2020-03-16 22:36:25 +1300

    We're outputting stuff!

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,21 +1,81 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
+import { graphql} from "gatsby"
 import SEO from "../components/seo"
+import 'bulma/css/bulma.css'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default class IndexPage extends React.Component{
+  
+  constructor(props){
+		super(props);
+		this.state = {
+      countries: props.data.countries.nodes,
+      selected_country: 'New Zealand',
+      
+		}
+  }
+  
+  
+  render(){
+    const {countries} = this.state
+    let active_country = countries.filter( (c) => c.country_name === this.state.selected_country )[0]
+    return (
+      <React.Fragment>
+        <SEO title="Home" />
+        <section className="hero is-info is-large">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                Flatten the curve
+              </h1>
+              <h2 className="subtitle">Why social distance is so important while our confirmed cases is low.</h2>
+              <div className="field is-grouped is-horizontal">
+                <div className="control">
+                  <div className="select is-large">
+                    <select value={this.state.selected_country} onChange={e => this.setState({selected_country: e.target.value})}>>
+                      <option>Choose Your country</option> 
+                      {countries.map( ({country_name }) => (
+                        <option key={country_name} value={country_name}>{country_name}</option>
+                      ))}
+                    </select>
+                  </div>
+                  
+                </div>
+                <div className="control">
+                  <button className="button is-large is-success">Go</button>
+                </div>
+                
+              </div>
+              <p className="is-size-4" style={{marginTop: '10px'}}>with {new Intl.NumberFormat().format(active_country.highest_confirmed)} confirmed cases</p>
+              <p className="is-size-6">We are only including countries with over 3,000,000 population and 3 or more confirmed cases</p>
+            </div>
+            
+          </div>
+        </section>
+          
+      </React.Fragment>
+    )
+  }
+  
+}
 
-export default IndexPage
+
+export const query = graphql`
+  query {
+    countries: allOutputJson(sort: {order: DESC, fields: highest_confirmed}, filter: {highest_confirmed: {gte: 3}, population: {gte: 3000000}}) {
+      nodes {
+        country_name
+        id
+        time_series {
+          date
+          confirmed
+          confirmed_per_mil
+          deaths
+          deaths_per_mil
+          recovered
+          recovered_per_mil
+        }
+        highest_confirmed
+      }
+    }
+  }
+`
--- processing/play.js
+++ processing/play.js
@@ -1,33 +1,66 @@
 const csv = require('csv-parser')
 const fs = require('fs')
-
 const _ = require('lodash')
 
-let confirmed = [];
-let deaths = []
-let recovered = []
-fs.createReadStream('data/time_series_19-covid-Confirmed.csv')
-.pipe(csv())
-.on('data', data => { confirmed.push(data) })
-.on('end', () => {    
-  fs.createReadStream('data/time_series_19-covid-Deaths.csv')
-  .pipe(csv())
-  .on('data', data => { deaths.push(data) })
-  .on('end', () => {    
-    fs.createReadStream('data/time_series_19-covid-Recovered.csv')
-    .pipe(csv())
-    .on('data', data => { recovered.push(data) })
-        .on('end', () => {    
-          mainThread()
-        });
-      });    
-  });
-
-
 const mainThread = () => {
-  countries = merge_object(restructure_inputs(confirmed), restructure_inputs(deaths), restructure_inputs(recovered))
-  console.log(JSON.stringify(countries, null, 2))
+  let confirmed = [];
+  let deaths = []
+  let recovered = []
+  let population_data = []
+
+  fs.createReadStream('data/time_series_19-covid-Confirmed.csv')
+  .pipe(csv())
+  .on('data', data => { confirmed.push(data) })
+  .on('end', () => {    
+    fs.createReadStream('data/time_series_19-covid-Deaths.csv')
+    .pipe(csv())
+    .on('data', data => { deaths.push(data) })
+    .on('end', () => {    
+      fs.createReadStream('data/time_series_19-covid-Recovered.csv')
+      .pipe(csv())
+      .on('data', data => { recovered.push(data) })
+      .on('end', () => {   
+        fs.createReadStream('data/population_world_bank.csv')
+        .pipe(csv())
+        .on('data', data => { population_data.push(data) })
+        .on('end', () => {  
+          countries = merge_object(restructure_inputs(confirmed), restructure_inputs(deaths), restructure_inputs(recovered))
+          countries = add_population_data(countries, population_data)
+
+          removed_key = _.map(countries, (country) => country)
+
+          fs.writeFile("./output.json", JSON.stringify(removed_key , null, 2), function(err) {
+            if(err) {
+              return console.log(err);
+            }
+            console.log("The file was saved!");
+          }); 
+        })
+      })
+    }); 
+  });
 }
+
+const add_population_data = (countries, population_data) => {
+  _.forEach(countries, (data, country_name) => {
+    population_data.forEach( (pop_data) => {
+      if( pop_data['Country Name'].toLowerCase()  == country_name.toLowerCase() ){
+        data.population = parseInt(pop_data['2018'])
+        data.time_series.forEach( (time) => {
+          if(time.confirmed)
+            time.confirmed_per_mil = time.confirmed / (data.population / 1000000)
+          if(time.deaths)
+            time.deaths_per_mil = time.deaths / (data.population / 1000000)
+          if(time.recovered)
+            time.recovered_per_mil = time.recovered / (data.population / 1000000)
+        })
+      }
+
+    })
+  })
+  return countries
+}
+
 const merge_object = (confirmed, deaths, recovered) => {
   let combined = {}
   
@@ -38,13 +71,13 @@ const merge_object = (confirmed, deaths, recovered) => {
     _.forEach(country, (val, key) => {
       if(validKey(key)){
         time_series.push( { 
-          date: key, confirmed: val 
+          date: key, confirmed: parseInt(val)
         } )
         if(val > highest_confirmed)
-          highest_confirmed = val
+          highest_confirmed = parseInt(val)
       }
     })
-    combined[country_name] = { time_series, highest_confirmed }
+    combined[country_name] = { country_name, time_series, highest_confirmed }
   })
 
   deaths.forEach( (country) => {
@@ -53,7 +86,7 @@ const merge_object = (confirmed, deaths, recovered) => {
       if(validKey(key)){
         combined[country_name].time_series.forEach( (time) => {
           if(key == time.date)
-            time.deaths = val
+            time.deaths = parseInt(val)
         })
       }
     })
@@ -65,7 +98,7 @@ const merge_object = (confirmed, deaths, recovered) => {
       if(validKey(key)){
         combined[country_name].time_series.forEach( (time) => {
           if(key == time.date)
-            time.recovered = val
+            time.recovered = parseInt(val)
         })
       }
     })
@@ -88,7 +121,6 @@ const restructure_inputs = (all_countries) => {
   
   return countries.filter(remove_negatives)
 }
-
 
 const combine_regions = (initial_regions) => {
   let combined = {}
@@ -119,3 +151,6 @@ const remove_negatives = (country) => {
 }
 
 const validKey = (key) => key != 'Country/Region' && key != 'Province/State' && key != 'Lat' && key != 'Long'
+
+
+mainThread()
