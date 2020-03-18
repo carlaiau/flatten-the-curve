31555c5d9beb211febeab17d558f37274663b29f
Carl Aiau <carl@ortus.co.nz>
2020-03-19 01:03:36 +1300

    home -> covid-19 in title

--- client/src/components/seo.js
+++ client/src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      titleTemplate={`COVID-19 | ${site.siteMetadata.title}`}
       meta={[
         {
           name: `description`,
