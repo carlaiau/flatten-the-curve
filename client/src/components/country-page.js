117deb97e3ff939409215ac3ef267bfc5052ce10
Carl Aiau <carl@ortus.co.nz>
2020-03-22 19:41:38 +1300

    Limit removed, and ability to alphabetical sort

--- client/src/components/country-page.js
+++ client/src/components/country-page.js
@@ -26,7 +26,6 @@ export default class CountryPage extends React.Component{
         field: 'confirmed',
         per: 'total',
         sort: 'worst',
-        limit: 60,
         width:  800,
         height: 182
     }
@@ -38,7 +37,7 @@ export default class CountryPage extends React.Component{
   
   render(){
     const {countries} = this.props.stateHook
-    const {selected_country, field, sort, per, limit} = this.state
+    const {selected_country, field, sort, per} = this.state
 
     let full_field_name = field === 'confirmed' ? 
       per === 'total' ? 'confirmed' : 'confirmed_per_mil' :
@@ -54,8 +53,7 @@ export default class CountryPage extends React.Component{
       countries, 
       active_country, 
       field: full_field_name, 
-      sort, 
-      limit 
+      sort
     })
     
     return (
